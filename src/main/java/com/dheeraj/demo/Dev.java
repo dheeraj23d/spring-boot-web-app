90f660037fb46aab27496c6766a4c0ff152fbd9c
Dheeraj <nizamdheerajkumar1@gmail.com>
2025-01-11 12:34:29 +0530

    @component demo

---
+++ src/main/java/com/dheeraj/demo/Dev.java
@@ -0,0 +1,10 @@
+package com.dheeraj.demo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dev {
+    public void build(){
+        System.out.println("Building on great project");
+    }
+}
