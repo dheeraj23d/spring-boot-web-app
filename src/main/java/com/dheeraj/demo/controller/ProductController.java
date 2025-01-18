cce156451f846d3ae8370b92c3062811bf72083b
Dheeraj <nizamdheerajkumar1@gmail.com>
2025-01-18 11:56:18 +0530

    getting the Product Object by its id from DB and send this image as response to the getImage frontend API

--- src/main/java/com/dheeraj/demo/controller/ProductController.java
+++ src/main/java/com/dheeraj/demo/controller/ProductController.java
@@ -4,6 +4,7 @@ import com.dheeraj.demo.model.Product;
 import com.dheeraj.demo.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -46,6 +47,20 @@ public class ProductController {
         }
         catch(Exception e){
             return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("product/{productId}/image")
+    public ResponseEntity<byte[]> getImageByProductId(@PathVariable int productId){
+        Product product = service.getProductById(productId);
+        if (product != null) {
+            byte[] imageFile = product.getImageFile();
+            return ResponseEntity.ok()
+                    .contentType(MediaType.valueOf(product.getImageType()))
+                    .body(imageFile);
+        }
+        else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
 }
