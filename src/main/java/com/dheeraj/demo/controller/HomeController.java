319e2fdbf34843b3bdbc79c0f93d2846080177fd
Dheeraj <nizamdheerajkumar1@gmail.com>
2025-01-19 14:04:57 +0530

    print session id from the info coming from client after successful login

--- src/main/java/com/dheeraj/demo/controller/HomeController.java
+++ src/main/java/com/dheeraj/demo/controller/HomeController.java
@@ -7,12 +7,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
     @RequestMapping("/")
-    public String greet(HttpServletRequest req){
-        return "Welcome to my Project" + req.getSession().getId();
+    public String greet(HttpServletRequest req){        // this request is handled after the login, this is after server assigning the session id to client. so we can tap into the object coming from client for session id
+        return "Welcome to my Project \n" + req.getSession().getId();
     }
 
     @RequestMapping("/about")
-    public String about(){
-        return "<h1>About Page</h1>";
+    public String about(HttpServletRequest req){
+        return "<h1>About Page</h1> " + req.getSession().getId();
     }
 }
