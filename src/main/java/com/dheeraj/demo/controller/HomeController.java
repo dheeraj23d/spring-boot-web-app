271e60c2d045819243e0e79000cda2bd733d3e08
Dheeraj <nizamdheerajkumar1@gmail.com>
2025-01-19 18:59:43 +0530

    print CSRF Token Cross-Site Request Forgery. which is generated every time the browser sends a request.

--- src/main/java/com/dheeraj/demo/controller/HomeController.java
+++ src/main/java/com/dheeraj/demo/controller/HomeController.java
@@ -1,6 +1,9 @@
 package com.dheeraj.demo.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +17,25 @@ public class HomeController {
     @RequestMapping("/about")
     public String about(HttpServletRequest req){
         return "<h1>About Page</h1> " + req.getSession().getId();
+    }
+
+/*  Example Workflow
+
+    Without CSRF Token:
+    1. User logs in, and the server sets a session ID in a cookie (JSESSIONID=xyz123).
+    2. The browser sends the session ID with each request, allowing the server to retrieve session data.
+    3. An attacker can exploit the session (if the session ID is stolen) or perform actions using CSRF.
+
+    With CSRF Token:
+    1. User logs in, and the server sets a session ID and generates a CSRF token (e.g., csrf123).
+    2. The CSRF token is embedded in a form as a hidden field or sent via a custom header.
+    3. When the form is submitted or the action is executed, the server checks:
+        Session ID (to verify the user).
+        CSRF token (to ensure the request is legitimate).
+    If both validations pass, the server processes the request.     */
+
+    @GetMapping("/csrf-token")
+    public CsrfToken getCsrfToken(HttpServletRequest request){
+        return (CsrfToken) request.getAttribute("_csrf");
     }
 }
