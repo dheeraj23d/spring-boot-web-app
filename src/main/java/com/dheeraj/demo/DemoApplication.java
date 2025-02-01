781fc615535dc02a67762addc5f95b6f88cfdb78
Dheeraj <nizamdheerajkumar1@gmail.com>
2025-02-02 00:22:10 +0530

    test commit

--- src/main/java/com/dheeraj/demo/DemoApplication.java
+++ src/main/java/com/dheeraj/demo/DemoApplication.java
@@ -9,7 +9,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args); //test
 	}
 
 }
